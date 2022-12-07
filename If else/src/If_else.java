06ea1ac9f302789552fc163891405a206292fcf2
cp457 <112082288+cp457@users.noreply.github.com>
2022-12-07 15:26:40 +0100

    Add files via upload

---
+++ If else/src/If_else.java
@@ -0,0 +1,14 @@
+public class If_else {
+
+    public static void main(String[] args) {
+
+        String name = "Christian";
+
+        if (name.length() > 9){
+            System.out.println( "it's even " + name.length() %2 );
+        }else{
+            System.out.println("it' odd");
+
+        }
+    }
+}
